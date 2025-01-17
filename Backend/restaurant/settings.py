4f90586dacd7afe6520efdb34bd6f9910917ef2c
abdul-09 <abdul.9.hussein@gmail.com>
2025-01-17 17:39:41 +0300

    deploy 1

--- Backend/restaurant/settings.py
+++ Backend/restaurant/settings.py
@@ -259,7 +259,12 @@ CORS_EXPOSE_HEADERS = ["Content-Type", "X-CSRFToken"]
 # EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 # DOMAIN = 'localhost:5173'
 # SITE_NAME = 'Restaurant App'
-
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = config('EMAIL_HOST')
+EMAIL_PORT = config('EMAIL_PORT', cast=int)
+EMAIL_USE_TLS = config('EMAIL_USE_TLS', cast=bool)
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
 
 LOGGING = {
     'version': 1,
